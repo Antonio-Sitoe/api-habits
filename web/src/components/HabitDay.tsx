689a525be7d2cb9787513057c380eb09e783be6f
Antonio Sitoe <antonio.sitoe@void.co.mz>
2023-06-28 14:04:59 +0200

    add

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as PoppOver from "@radix-ui/react-popover";
+import * as CheckBox from "@radix-ui/react-checkbox";
 import { ProgressBar } from "./ProgressBar";
 import clsx from "clsx";
+import { Check } from "phosphor-react";
 
 interface HabitDayProps {
   completed: number;
@@ -34,6 +36,19 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
           </span>
 
           <ProgressBar progress={45} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <CheckBox.Root className="flex items-center gap-3 group:">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
+                <CheckBox.Indicator>
+                  <Check size={20} className="text-white" />
+                </CheckBox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2 Litros de Agua
+              </span>
+            </CheckBox.Root>
+          </div>
           <PoppOver.Arrow height={8} width={16} className="fill-zinc-900" />
         </PoppOver.Content>
       </PoppOver.Portal>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,5 @@
 import { Check } from "phosphor-react";
+import * as CheckBox from "@radix-ui/react-checkbox";
 
 function NewHabitForm() {
   return (
@@ -17,6 +18,19 @@ function NewHabitForm() {
       <label htmlFor="title" className="font-semibold leading-tight mt-4">
         Qual e a recorrencia
       </label>
+
+      <div className="mt-6 flex flex-col gap-3">
+        <CheckBox.Root className="flex items-center gap-3 group:">
+          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
+            <CheckBox.Indicator>
+              <Check size={20} className="text-white" />
+            </CheckBox.Indicator>
+          </div>
+          <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+            Segunda-Feira
+          </span>
+        </CheckBox.Root>
+      </div>
 
       <button className="flex items-center mt-6 rounded-lg p-4 gap-3 font-semibold bg-green-600 justify-center hover:bg-green-500">
         <Check size={20} weight="bold" />
