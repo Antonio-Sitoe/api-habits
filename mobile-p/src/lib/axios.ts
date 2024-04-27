23c2cc32a2f52e937e76a6e4e40f2b4c30331c0f
Antonio Sitoe <antoniositoehl@gmail.com>
2024-04-27 14:19:38 +0200

    changes folder estrtuture

--- mobile-p/src/lib/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  // baseURL: "http://192.168.43.84:3333",
-  baseURL: "http://192.168.43.84:4000",
-  // baseURL: "http://192.168.43.84:3333",
-});
